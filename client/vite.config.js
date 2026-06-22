3766b975be470ec0beec115d8f459d6d6798c841
Muhammad Tabish Musheer <tabishmusheer17@gmail.com>
2026-06-23 01:50:30 +0500

    Fixed merge conflicts in package.json and vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -13,12 +13,5 @@ export default defineConfig({
         secure: false,
       }
     }
-=======
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
->>>>>>> Amira-branch
   }
 })
