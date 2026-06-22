03156e965474eb7596715d3781070314c597e12e
Muhammad Tabish Musheer <tabishmusheer17@gmail.com>
2026-06-23 01:41:21 +0500

    Added frontend API configurations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
+
+// Set the base URL for all API requests
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
